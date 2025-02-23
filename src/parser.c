d8789aa268235f654d76262b8393b2c9e582a496
mertensnova <amr8644@gmail.com>
2025-02-23 12:12:55 +0300

    Finished rewriting get_args() function

--- src/parser.c
+++ src/parser.c
@@ -51,9 +51,10 @@ char **get_args(char *input) {
 
   char *word = (char *)malloc((30 + 1) * sizeof(char));
   char **args = (char **)malloc(200 * sizeof(char *));
- 
+
   int word_counter = 0;
   int args_counter = 0;
+  int saver = 0;
 
   if (word == NULL) {
     fprintf(stderr, "Memory allocation failed\n");
@@ -64,30 +65,24 @@ char **get_args(char *input) {
     if (input[i] == '"') {
       continue;
     }
+
     if (isspace(input[i]) == 0) {
       word[word_counter++] = input[i];
     } else {
-      word[word_counter + 1] = '\0';
-      args[args_counter] = (char *)malloc((10 + 1) * sizeof(char));
+      word[word_counter] = '\0';
+      args[args_counter] = (char *)malloc((word_counter + 1) * sizeof(char));
+
       if (args[args_counter] == NULL) {
         fprintf(stderr, "Memory allocation failed for word %d\n", i);
         exit(1);
       }
-
-      args[args_counter] = word;
-      args_counter++;
+      strcpy(args[args_counter++], word);
       word_counter = 0;
     }
   };
 
-
-
-
-  for (size_t i = 0; i < 10; i++) {
-
-    printf("%s", args[i]);
-  }
-
+  args[args_counter] = (char *)malloc((strlen(word) + 1) * sizeof(char));
+  strcpy(args[args_counter++], word);
 
   return args;
 };
